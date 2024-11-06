aa114b518755671b0555a93fe6dd44da8f01b662
TonyBean <38479405+NonsensicalLab@users.noreply.github.com>
2024-11-06 11:27:42 +0800

    ab包管理添加动态修改url逻辑

--- Runtime/Core/Service/AssetBundleService/AssetBundleService.cs
+++ Runtime/Core/Service/AssetBundleService/AssetBundleService.cs
@@ -1,23 +1,27 @@
-using NonsensicalKit.Core.Log;
 using System;
 using System.Collections;
 using System.Collections.Generic;
 using System.IO;
+using NonsensicalKit.Core.Log;
 using UnityEngine;
 using UnityEngine.Networking;
-using UnityEngine.Serialization;
+using Object = UnityEngine.Object;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
 
 namespace NonsensicalKit.Core.Service.Asset
 {
     [ServicePrefab("Services/AssetBundleService")]
     public class AssetBundleService : NonsensicalMono, IMonoService
     {
-        [SerializeField][FormerlySerializedAs("m_baseUrl")] private string m_runtimeBaseUrl;  //基础url
+        [SerializeField] private string m_runtimeBaseUrl; //基础url
         [SerializeField] private string m_editorBasePath = "..\\AssetBundles";
         [SerializeField] private int m_version = 10000;
-        [SerializeField] private string[] m_preDownloadBundles;   //需要提前下载的ab包
+        [SerializeField] private string[] m_preDownloadBundles; //需要提前下载的ab包
 
-        public bool IsReady { get; set; }
+        public bool IsReady { get; private set; }
 
         public Action InitCompleted { get; set; }
 
@@ -26,33 +30,64 @@ namespace NonsensicalKit.Core.Service.Asset
         public Action<string, string, float> OnResourceLoading { get; set; }
         public Action<string, string> OnResourceCompleted { get; set; }
 
-        public bool IsLoading => LoadBundleCount > 0;       //当前是否正在加载包
+        public bool IsLoading => LoadBundleCount > 0; //当前是否正在加载包
 
-        public int LoadBundleCount { get; private set; }    //当前加载的包的数量
+        public int LoadBundleCount { get; private set; } //当前加载的包的数量
 
-        private string _rootUrl;    //基础url拼接平台字符串得出的根路径
+        private string _rootUrl; //基础url拼接平台字符串得出的根路径
 
-        private Dictionary<string, AssetBundleContext> _assstBundleDic = new Dictionary<string, AssetBundleContext>();      //key是包名，value是ab包上下文信息
+        private readonly Dictionary<string, AssetBundleContext> _assstBundleDic = new Dictionary<string, AssetBundleContext>(); //key是包名，value是ab包上下文信息
 
         protected void Awake()
         {
 #if UNITY_EDITOR
             //读取当前版本并赋值到预制体中
-            int version = UnityEditor.EditorPrefs.GetInt("assetBundle_Version", 10000);
+            int version = EditorPrefs.GetInt("assetBundle_Version", 10000);
             if (m_version < version)
             {
                 m_version = version;
                 var service = Resources.Load<AssetBundleService>("Services/AssetBundleService");
                 service.m_version = version;
-                UnityEditor.EditorUtility.SetDirty(service);
-                UnityEditor.PrefabUtility.RecordPrefabInstancePropertyModifications(service.gameObject);
-                UnityEditor.PrefabUtility.SavePrefabAsset(service.gameObject);
+                EditorUtility.SetDirty(service);
+                PrefabUtility.RecordPrefabInstancePropertyModifications(service.gameObject);
+                PrefabUtility.SavePrefabAsset(service.gameObject);
             }
             else if (m_version > version)
             {
-                UnityEditor.EditorPrefs.SetInt("assetBundle_Version", m_version);
+                EditorPrefs.SetInt("assetBundle_Version", m_version);
             }
 #endif
+            AutoInit();
+        }
+
+        public void Init(string baseUrl)
+        {
+            if (string.IsNullOrEmpty(baseUrl)) return;
+            _rootUrl = Path.Combine(baseUrl, PlatformInfo.GetPlaformFolderName());
+
+            StartCoroutine(InitCor());
+        }
+
+        private void AutoInit()
+        {
+            if (PlatformInfo.IsEditor)
+            {
+                if (string.IsNullOrEmpty(m_editorBasePath))
+                {
+                    LogCore.Error("未配置编辑时ab包路径");
+                }
+
+                _rootUrl = Path.Combine(Application.dataPath, m_editorBasePath, PlatformInfo.GetPlaformFolderName());
+            }
+            else
+            {
+                if (string.IsNullOrEmpty(m_runtimeBaseUrl))
+                {
+                    return;
+                }
+
+                _rootUrl = Path.Combine(m_runtimeBaseUrl, PlatformInfo.GetPlaformFolderName());
+            }
 
             StartCoroutine(InitCor());
         }
@@ -62,24 +97,19 @@ namespace NonsensicalKit.Core.Service.Asset
         /// </summary>
         private IEnumerator InitCor()
         {
-            if (PlatformInfo.IsEditor)
-            {
-                _rootUrl = Path.Combine( Application.dataPath, m_editorBasePath, PlatformInfo.GetPlaformFolderName());
-            }
-            else
-            {
-                _rootUrl = Path.Combine(m_runtimeBaseUrl, PlatformInfo.GetPlaformFolderName());
-            }
+            IsReady = false;
 
             var assetBundleCreateRequest = UnityWebRequestAssetBundle.GetAssetBundle(Path.Combine(_rootUrl, PlatformInfo.GetPlaformFolderName()));
             yield return assetBundleCreateRequest.SendWebRequest();
-            AssetBundle assetBundle = (assetBundleCreateRequest.downloadHandler as DownloadHandlerAssetBundle).assetBundle;
+            AssetBundle assetBundle = (assetBundleCreateRequest.downloadHandler as DownloadHandlerAssetBundle)?.assetBundle;
 
             if (assetBundle == null)
             {
                 LogCore.Warning("未找到AssetBundles:" + _rootUrl);
                 yield break;
             }
+
+            _assstBundleDic.Clear();
 
             var manifestRequest = assetBundle.LoadAssetAsync<AssetBundleManifest>("AssetBundleManifest");
             yield return manifestRequest;
@@ -91,6 +121,7 @@ namespace NonsensicalKit.Core.Service.Asset
             {
                 _assstBundleDic.Add(item, new AssetBundleContext(item, assetBundleManifest.GetDirectDependencies(item)));
             }
+
 
             IsReady = true;
             InitCompleted?.Invoke();
@@ -116,6 +147,7 @@ namespace NonsensicalKit.Core.Service.Asset
                 {
                     yield return null;
                 }
+
                 var _crtPreDownloadBundleName = m_preDownloadBundles[i];
                 LogCore.Info("开始提前下载包：" + _crtPreDownloadBundleName);
                 LoadAssetBundle(_crtPreDownloadBundleName, null, null);
@@ -123,8 +155,10 @@ namespace NonsensicalKit.Core.Service.Asset
                 {
                     yield return null;
                 }
+
                 UnloadUnusedBundle(_crtPreDownloadBundleName);
             }
+
             LogCore.Info("提前下载完成");
         }
 
@@ -154,7 +188,7 @@ namespace NonsensicalKit.Core.Service.Asset
         /// <param name="onLoading"></param>
         public void LoadAssetBundle(string bundlePath, Action<string> onComplete = null, Action<string, float> onLoading = null, Action<string> onLoaded = null)
         {
-            bundlePath=bundlePath.ToLower();
+            bundlePath = bundlePath.ToLower();
             LogCore.Info("加载ab包：" + bundlePath);
             if (_assstBundleDic.ContainsKey(bundlePath) == false)
             {
@@ -173,6 +207,7 @@ namespace NonsensicalKit.Core.Service.Asset
                 catch (Exception)
                 {
                 }
+
                 return;
             }
 
@@ -215,8 +250,7 @@ namespace NonsensicalKit.Core.Service.Asset
                 crtProcess = request.downloadProgress;
                 _assstBundleDic[bundlePath].OnLoading?.Invoke(bundlePath, crtProcess);
                 OnBundleLoading?.Invoke(bundlePath, crtProcess);
-            }
-            while (!request.isDone);
+            } while (!request.isDone);
 
             AssetBundle assetBundle = DownloadHandlerAssetBundle.GetContent(request);
 
@@ -231,7 +265,9 @@ namespace NonsensicalKit.Core.Service.Asset
             {
                 _assstBundleDic[bundlePath].OnLoaded?.Invoke(bundlePath);
             }
-            catch (Exception) { }
+            catch (Exception)
+            {
+            }
 
             _assstBundleDic[bundlePath].OnLoaded = null;
             _assstBundleDic[bundlePath].IsLoaded = true;
@@ -247,6 +283,7 @@ namespace NonsensicalKit.Core.Service.Asset
                 _assstBundleDic[item].DependencieCount++;
                 LoadAssetBundle(item, null, null, (p) => { completeCount++; });
             }
+
             while (completeCount < dependencies.Length)
             {
                 yield return null;
@@ -262,7 +299,9 @@ namespace NonsensicalKit.Core.Service.Asset
                 _assstBundleDic[bundlePath].OnLoadCompleted?.Invoke(bundlePath);
                 OnBundleCompleted?.Invoke(bundlePath);
             }
-            catch (Exception) { }
+            catch (Exception)
+            {
+            }
 
             _assstBundleDic[bundlePath].OnLoadCompleted = null;
 
@@ -278,7 +317,8 @@ namespace NonsensicalKit.Core.Service.Asset
         /// <param name="resourceName">资源名</param>
         /// <param name="bundlePath">包名</param>
         /// <param name="onCompleted">完成回调</param>
-        public void LoadResource<T>(string resourceName, string bundlePath, Action<string, string, T> onCompleted, Action<string, string, float> onLoading = null) where T : UnityEngine.Object
+        public void LoadResource<T>(string resourceName, string bundlePath, Action<string, string, T> onCompleted,
+            Action<string, string, float> onLoading = null) where T : Object
         {
             bundlePath = bundlePath.ToLower();
 
@@ -309,12 +349,14 @@ namespace NonsensicalKit.Core.Service.Asset
         /// <param name="onCompleted"></param>
         /// <param name="onLoading"></param>
         /// <returns></returns>
-        private IEnumerator WaitAssetBundleLoad<T>(string resourceName, string bundlePath, Action<string, string, T> onCompleted, Action<string, string, float> onLoading) where T : UnityEngine.Object
+        private IEnumerator WaitAssetBundleLoad<T>(string resourceName, string bundlePath, Action<string, string, T> onCompleted,
+            Action<string, string, float> onLoading) where T : Object
         {
             while (_assstBundleDic[bundlePath].IsLoadCompleted == false)
             {
                 yield return null;
             }
+
             _assstBundleDic[bundlePath].LoadCount++;
             StartCoroutine(LoadResourceCoroutine<T>(resourceName, bundlePath, _assstBundleDic[bundlePath].AssetBundlePack, onCompleted, onLoading));
         }
@@ -327,7 +369,8 @@ namespace NonsensicalKit.Core.Service.Asset
         /// <param name="assetBundle"></param>
         /// <param name="onCompleted"></param>
         /// <returns></returns>
-        private IEnumerator LoadResourceCoroutine<T>(string resourceName, string bundlePath, AssetBundle assetBundle, Action<string, string, T> onCompleted, Action<string, string, float> onLoading) where T : UnityEngine.Object
+        private IEnumerator LoadResourceCoroutine<T>(string resourceName, string bundlePath, AssetBundle assetBundle, Action<string, string, T> onCompleted,
+            Action<string, string, float> onLoading) where T : Object
         {
             AssetBundleRequest assetBundleRequest = assetBundle.LoadAssetAsync<T>(resourceName);
 
@@ -343,7 +386,6 @@ namespace NonsensicalKit.Core.Service.Asset
 
                     onLoading(bundlePath, resourceName, assetBundleRequest.progress);
                     OnResourceLoading?.Invoke(bundlePath, resourceName, assetBundleRequest.progress);
-
                 } while (!assetBundleRequest.isDone);
             }
 
@@ -414,6 +456,7 @@ namespace NonsensicalKit.Core.Service.Asset
                         bundle.DependencieCount = 0;
                     }
                 }
+
                 bundle.IsLoading = false;
                 bundle.IsLoaded = false;
                 bundle.IsLoadCompleted = false;
@@ -422,7 +465,6 @@ namespace NonsensicalKit.Core.Service.Asset
                 bundle.OnLoadCompleted = null;
                 bundle.OnLoaded = null;
                 bundle.OnLoading = null;
-
             }
         }
 
@@ -451,20 +493,20 @@ namespace NonsensicalKit.Core.Service.Asset
 
         private class AssetBundleContext
         {
-            public string BundlePath;           //包的名称
-            public string[] Dependencies;       //直接依赖的包名称
+            public string BundlePath; //包的名称
+            public string[] Dependencies; //直接依赖的包名称
 
             public AssetBundle AssetBundlePack; //ab包
 
-            public bool IsLoading;              //是否正在进行加载
-            public bool IsLoaded;               //是否已经加载完成
-            public bool IsLoadCompleted;        //是否已经完全加载完成
-            public int LoadCount = 0;           //包内对象加载的次数
-            public int DependencieCount = 0;    //被其他包依赖加载的次数
+            public bool IsLoading; //是否正在进行加载
+            public bool IsLoaded; //是否已经加载完成
+            public bool IsLoadCompleted; //是否已经完全加载完成
+            public int LoadCount = 0; //包内对象加载的次数
+            public int DependencieCount = 0; //被其他包依赖加载的次数
 
-            public Action<string> OnLoadCompleted;       //完全加载完成事件
-            public Action<string> OnLoaded;             //加载完成事件，此时依赖包可能尚未加载
-            public Action<string, float> OnLoading;     //加载中进度事件，会使用一个0到1的进度调用此事件代表下载进度
+            public Action<string> OnLoadCompleted; //完全加载完成事件
+            public Action<string> OnLoaded; //加载完成事件，此时依赖包可能尚未加载
+            public Action<string, float> OnLoading; //加载中进度事件，会使用一个0到1的进度调用此事件代表下载进度
 
             public AssetBundleContext(string bundleName, string[] dependencies)
             {
