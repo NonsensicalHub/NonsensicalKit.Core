f395baed42834ccfba5763b0857b532793ae6010
TonyBean <38479405+TonyBean-Dong@users.noreply.github.com>
2024-12-04 14:57:43 +0800

    逻辑节点树移至Core

---
+++ Runtime/Tools/LogicNodeTreeSystem/Core/LogicNodeEnum.cs
@@ -0,0 +1,13 @@
+using NonsensicalKit.Core;
+
+namespace NonsensicalKit.Tools.LogicNodeTreeSystem
+{
+    [AggregatorEnum]
+    public enum LogicNodeEnum
+    {
+        SwitchNode = 10200,
+        InitNode,
+        NodeEnter,
+        NodeExit,
+    }
+}
